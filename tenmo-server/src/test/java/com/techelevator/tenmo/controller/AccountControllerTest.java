52981d4d253b662cc4c8a4b021e7a9c913ff62fe
ChristinaGraham <cgraham74@gmail.com>
2022-06-08 22:57:32 -0500

    Added Unit tests for Account and Transfer controllers

--- tenmo-server/src/test/java/com/techelevator/tenmo/controller/AccountControllerTest.java
+++ tenmo-server/src/test/java/com/techelevator/tenmo/controller/AccountControllerTest.java
@@ -1,4 +1,38 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.repositories.AccountRepository;
+
+import com.techelevator.tenmo.services.AccountService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
 public class AccountControllerTest {
+
+    @MockBean
+    private AccountRepository repository;
+
+    @Autowired
+    private AccountController controller;
+
+    @Autowired
+    AccountService service;
+
+    @Test
+    public void created_repository_not_null(){
+        Assertions.assertNotNull(repository);
+    }
+
+    @Test
+    public void created_controller_not_null(){
+        Assertions.assertNotNull(controller);
+    }
+
+    @Test
+    public void created_service_not_null() {
+        Assertions.assertNotNull(service);
+    }
 }
--- tenmo-server/src/test/java/com/techelevator/tenmo/controller/TransferControllerTest.java
+++ tenmo-server/src/test/java/com/techelevator/tenmo/controller/TransferControllerTest.java
@@ -1,5 +1,38 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.repositories.TransferRepository;
+import com.techelevator.tenmo.services.TransferService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
 public class TransferControllerTest {
 
+    @MockBean
+    private
+    TransferRepository transferRepository;
+
+    @Autowired
+    TransferController transferController;
+
+    @Autowired
+    TransferService transferService;
+
+    @Test
+    public void created_repository_not_null(){
+        Assertions.assertNotNull(transferRepository);
+    }
+
+    @Test
+    public void created_controller_not_null(){
+        Assertions.assertNotNull(transferController);
+    }
+
+    @Test
+    public void created_service_not_null(){
+        Assertions.assertNotNull(transferService);
+    }
 }
