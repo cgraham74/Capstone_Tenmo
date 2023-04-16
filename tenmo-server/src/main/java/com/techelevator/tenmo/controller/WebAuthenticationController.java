3660ae8d80542ee57605fb514e2c296dfd030637
ChristinaGraham <cgraham74@gmail.com>
2023-04-16 09:29:11 -0500

    Added and clarified documentation, created static variables for pages

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/WebAuthenticationController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/WebAuthenticationController.java
@@ -26,12 +26,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-
+/**
+ *
+ *
+ * @author Christina Graham
+ */
 @PreAuthorize("permitAll()")
 @CrossOrigin
 @Controller
 @Data
 public class WebAuthenticationController {
+
+    private  static final String registerPage = "register";
+    private static final String errorMessage ="errorMessage";
 
     @Autowired
     private final TokenProvider tokenProvider;
@@ -50,6 +57,19 @@ public class WebAuthenticationController {
 
     }
 
+    /**
+     * This method displays the login form to the user by populating the Model object with the necessary
+     * attributes and returning the view name for the login form. It handles incoming GET requests at the root path ("/").
+     *
+     * @param model The Model object is used to pass attributes (data) from the controller to the view.
+     *              It serves as a container for the data that needs to be displayed on the view.
+     * @return The view name for the login form, which is "login" in this case.
+     *
+     * Note:
+     * Ensure that the "login" view name returned by this method corresponds to the actual login form view
+     * in your application. Also, make sure to properly configure the LoginDTO class to
+     * capture the user's login credentials from the form.
+     */
     @GetMapping("/")
     public String showLogInForm(Model model) {
         model.addAttribute("message", "Log in");
@@ -60,11 +80,11 @@ public class WebAuthenticationController {
     @GetMapping("/register")
     public String showRegisteredUserForm(Model model) {
         model.addAttribute("message", "Create a new account");
-        return "register";
+        return registerPage;
     }
 
     /**
-     * When the user enters their username and password in the log in form
+     * When the user enters their username and password in the login form
      * The form action calls this endpoint to authenticate the user and
      * add the user to session storage
      *
@@ -93,21 +113,19 @@ public class WebAuthenticationController {
             modelAndView.addObject("loginResponse", loginResponse);
             modelAndView.addObject("user", loginResponse.getUser());
             modelAndView.addObject("jwt", loginResponse.getToken());
-            System.out.println("longin Response user: " + loginResponse.getUser());
+            System.out.println("login Response user: " + loginResponse.getUser());
 
             session.setAttribute("user", loginResponse.getUser());
             return modelAndView;
         } catch (UserNotActivatedException e) {
             ModelAndView modelAndView = new ModelAndView("error");
-            modelAndView.addObject("errorMessage", e.getMessage());
+            modelAndView.addObject(errorMessage, e.getMessage());
             return modelAndView;
         } catch (Exception e) {
             ModelAndView modelAndView = new ModelAndView("error");
-            modelAndView.addObject("errorMessage", "Invalid username or password");
+            modelAndView.addObject(errorMessage, "Invalid username or password");
             return modelAndView;
         }
-
-
     }
 
     /**
@@ -121,16 +139,37 @@ public class WebAuthenticationController {
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute(name = "registerUserDTO") RegisterUserDTO newUser, Model model) {
         if (!newUser.getPassword().equals(newUser.getPasswordConfirmation())) {
-            model.addAttribute("errorMessage", "Passwords do not match");
-            return "register";
+            model.addAttribute(errorMessage, "Passwords do not match");
+            return registerPage;
         }
         if (!userDao.create(newUser.getUsername(), newUser.getPassword())) {
-            model.addAttribute("errorMessage", "User registration failed");
-            return "register";
+            model.addAttribute(errorMessage, "User registration failed");
+            return registerPage;
         }
         return "login";
     }
 
+    /**
+     *  This function logs off the user from the current session and redirects
+     *  them to the root path of the application. The function can handle both GET and POST requests.
+     *
+     * @param request The incoming HTTP request object that contains information about the
+     *                client's request, such as request headers, path, and parameters.
+     * @param response The outgoing HTTP response object that the server sends back to the client.
+     *                 It contains information such as status code, response headers, and response body.
+     * @return the redirection path, which is the root path ("/") of the application.
+     *
+     * Usage:
+     * To log off a user, send an HTTP request (either GET or POST) to the "/logoff" path.
+     * The function checks if the user is authenticated, and if so, logs the user off using the
+     * SecurityContextLogoutHandler().logout() method. After logging off, the user is
+     * redirected to the root path of the application.
+     *
+     * Note:
+     * Make sure to properly configure the security context and SecurityContextLogoutHandler to ensure that
+     * the logout process works as intended. Additionally, ensure that the "/logoff" path is allowed for
+     * both authenticated and unauthenticated users in your application's security configuration
+     */
     @RequestMapping(value = "/logoff", method = {RequestMethod.GET, RequestMethod.POST})
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
